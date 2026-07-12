28e725627ae79c0f0b0b3738efefd7814556550f
nickeyprod <nickeyprod@gmail.com>
2026-07-12 20:46:19 +0300

    Add img

--- distr/client/src/layouts/tables/index.js
+++ distr/client/src/layouts/tables/index.js
@@ -13,6 +13,8 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
+import { useState } from "react";
+
 // @mui material components
 import Card from "@mui/material/Card";
 
@@ -30,47 +32,99 @@ import Table from "examples/Tables/Table";
 import authorsTableData from "layouts/tables/data/authorsTableData";
 import projectsTableData from "layouts/tables/data/projectsTableData";
 
+// AG Grid
+import { AllCommunityModule } from 'ag-grid-community';
+import { AgGridProvider, AgGridReact } from 'ag-grid-react';
+
+const modules = [AllCommunityModule];
+
 function Tables() {
   const { columns, rows } = authorsTableData;
   const { columns: prCols, rows: prRows } = projectsTableData;
+
+  // Row Data: The data to be displayed.
+    const [rowData, setRowData] = useState([
+      { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+
+      { make: "Ford", model: "F-Series", price: 33850, electric: false },
+
+      { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+
+      { make: "Mercedes", model: "EQA", price: 48890, electric: true },
+
+      { make: "Fiat", model: "500", price: 15774, electric: false },
+
+      { make: "Nissan", model: "Juke", price: 20675, electric: false },
+    ]);
+
+    // Column Definitions: Defines the columns to be displayed.
+    const [colDefs, setColDefs] = useState([
+        { field: "make", flex: 1 },
+        { field: "model", flex: 1 },
+        { field: "price", flex: 1 },
+        { field: "electric", flex: 1 }
+    ]);
 
   return (
     <DashboardLayout>
       <DashboardNavbar />
       <ArgonBox py={3}>
+        
         <ArgonBox mb={3}>
+          
+
+          
           <Card>
+            <ArgonBox display="flex" justifyContent="space-between" alignItems="center" p={3}>
+              <ArgonTypography variant="h6">1 таблица</ArgonTypography>
+            </ArgonBox>
+            <AgGridProvider modules={modules}>
+              {/* Your AgGridReact components go here */}
+              <div style={{ height: "100%"}}>
+                <AgGridReact
+                    domLayout="autoHeight"
+                    rowData={rowData}
+                    columnDefs={colDefs}
+                />
+            </div>
+          </AgGridProvider>
+          </Card>
+          
+          <ArgonBox>
+            <Card style={{ marginTop: 20 }}>
+              <ArgonBox display="flex" justifyContent="space-between" alignItems="center" p={3}>
+                <ArgonTypography variant="h6">2 таблица</ArgonTypography>
+              </ArgonBox>
+
+              <AgGridProvider modules={modules}>
+                <div style={{ height: "100%"}}>
+                  <AgGridReact
+                      domLayout="autoHeight"
+                      rowData={rowData}
+                      columnDefs={colDefs}
+                  />
+                </div>
+              </AgGridProvider>
+            </Card>
+          </ArgonBox>
+
+          <Card style={{ marginTop: 20 }}>
             <ArgonBox display="flex" justifyContent="space-between" alignItems="center" p={3}>
               <ArgonTypography variant="h6">Authors table</ArgonTypography>
             </ArgonBox>
             <ArgonBox
-              sx={{
-                "& .MuiTableRow-root:not(:last-child)": {
-                  "& td": {
-                    borderBottom: ({ borders: { borderWidth, borderColor } }) =>
-                      `${borderWidth[1]} solid ${borderColor}`,
-                  },
-                },
-              }}
+          
             >
               <Table columns={columns} rows={rows} />
             </ArgonBox>
           </Card>
         </ArgonBox>
+
         <Card>
           <ArgonBox display="flex" justifyContent="space-between" alignItems="center" p={3}>
             <ArgonTypography variant="h6">Projects table</ArgonTypography>
           </ArgonBox>
-          <ArgonBox
-            sx={{
-              "& .MuiTableRow-root:not(:last-child)": {
-                "& td": {
-                  borderBottom: ({ borders: { borderWidth, borderColor } }) =>
-                    `${borderWidth[1]} solid ${borderColor}`,
-                },
-              },
-            }}
-          >
+          <ArgonBox>
             <Table columns={prCols} rows={prRows} />
           </ArgonBox>
         </Card>
